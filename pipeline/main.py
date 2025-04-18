73c7cc6d9a893e841e140816f0cc1814a38da118
zzq89757 <1604653835@qq.com>
2025-04-18 02:15:04 +0000

    qc test

--- pipeline/main.py
+++ pipeline/main.py
@@ -6,9 +6,27 @@ from pysam import AlignmentFile, AlignedSegment
 import pandas as pd
 
 
-def quality_check(fq_path: str, clean_data_path: str) -> None:
-    # 运行NanoFilt 并处理报告
-    system(f"gunzip -c {fq_path} | NanoFilt -q 12 -l 1000 --headcrop 50 --tailcrop 50 | gzip > clean_reads.fq.gz")
+def extract_qc_info(qc_res_path: str) -> None:
+    ...
+
+
+def quality_check(fq_path: str, output_data_path: str) -> None:
+    qc_res_path = f"{output_data_path}/qc_summary/"
+    if not Path(qc_res_path).exists():
+        Path(qc_res_path).mkdir(exist_ok=1,parents=1)
+    # 过滤接头
+    porechop_cmd = f"porechop -i {fq_path} -o {output_data_path}/adapter_trimmed.fq --threads 24 \
+         --adapter_threshold 85 --extra_end_trim 20 \
+         --discard_middle"
+    # 运行NanoFilt 过滤长度低于阈值以及低质量数据
+    nanofilt_cmd = f"NanoFilt -q 10 -l 500 --headcrop 50 --tailcrop 50 {output_data_path}/adapter_trimmed.fq > {output_data_path}/clean_reads.fq"
+    # 运行Nanoplot 生成qc报告
+    nanoplot_cmd = f"NanoPlot -t 24 --fastq {output_data_path}/clean_reads.fq -o {qc_res_path}"
+    system(porechop_cmd)
+    system(nanofilt_cmd)
+    system(nanoplot_cmd)
+    # 提取qc 信息
+    extract_qc_info(qc_res_path)
     
     
 def sgRNA_detective() -> None:
@@ -39,7 +57,7 @@ def parse_alignment_result(bam_file_path: str, res_table_path: str) -> None:
     
 
 def main() -> None:
-    quality_check("","")
+    quality_check("../painted_fq/C2931XKUG0-1_c-ps232691-1.fastq","./test/")
     
     
 if __name__ == "__main__":
